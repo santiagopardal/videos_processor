909c028976f5687bcdbe2a655673677287d39898
Santiago Pardal <santiagopardal@outlook.com>
2024-04-24 12:37:59 -0300

    Initial changes, add RabbitMQ permanent listener

---
+++ src/consumer/temporal_videos_consumer.rs
@@ -0,0 +1,40 @@
+use amqprs::channel::{BasicAckArguments, Channel};
+use amqprs::consumer::AsyncConsumer;
+use amqprs::{BasicProperties, Deliver};
+use async_trait::async_trait;
+use std::str;
+
+pub struct TemporalVideosConsumer;
+
+impl TemporalVideosConsumer {
+    pub fn new() -> Self {
+        return Self;
+    }
+}
+
+#[async_trait]
+impl AsyncConsumer for TemporalVideosConsumer {
+    async fn consume(
+        &mut self,
+        channel: &Channel,
+        deliver: Deliver,
+        _basic_properties: BasicProperties,
+        content: Vec<u8>,
+    ) {
+        let s: &str = match str::from_utf8(&content) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+
+        let json_data = json::parse(s).unwrap();
+
+        println!("Message is: '{}'", s);
+        println!("JSON is: '{}'", json_data);
+
+        let args = BasicAckArguments::new(deliver.delivery_tag(), false);
+        channel.basic_ack(args).await.unwrap();
+
+        println!("ACKd message!");
+        println!();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use amqprs::{
+    callbacks::{
+        DefaultChannelCallback,
+        DefaultConnectionCallback
+    },
+    channel::{
+        BasicConsumeArguments,
+        QueueBindArguments,
+        QueueDeclareArguments,
+    },
+    connection::{
+        Connection,
+        OpenConnectionArguments
+    }
+};
+use tokio::sync::Notify;
+use tracing_subscriber::{fmt, prelude::*};
+pub mod consumer;
+use consumer::temporal_videos_consumer::TemporalVideosConsumer;
+
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
+async fn main() {
+    let rabbit_host = env::var("RABBIT_HOST").unwrap();
+    let rabbit_user = env::var("RABBIT_USER").unwrap();
+    let rabbit_password = env::var("RABBIT_PASSWORD").unwrap();
+
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .try_init()
+        .ok();
+
+    let connection = Connection::open(
+        &OpenConnectionArguments::new(
+            rabbit_host.as_str(),
+            5672,
+            rabbit_user.as_str(),
+            rabbit_password.as_str(),
+        )
+    )
+        .await
+        .unwrap();
+
+    connection
+        .register_callback(DefaultConnectionCallback)
+        .await
+        .unwrap();
+
+    let channel = connection.open_channel(None).await.unwrap();
+    channel
+        .register_callback(DefaultChannelCallback)
+        .await
+        .unwrap();
+
+    let (queue_name, _, _) = channel
+        .queue_declare(QueueDeclareArguments::new("testing_queue"))
+        .await
+        .unwrap()
+        .unwrap();
+
+    let routing_key = "amqprs.example";
+    let exchange_name = "test_exchange";
+    channel
+        .queue_bind(QueueBindArguments::new(
+            &queue_name,
+            exchange_name,
+            routing_key,
+        ))
+        .await
+        .unwrap();
+
+    let args = BasicConsumeArguments::new(&queue_name, "basic_consumer")
+        .manual_ack(true)
+        .finish();
+
+    channel
+        .basic_consume(TemporalVideosConsumer::new(), args)
+        .await
+        .unwrap();
+
+    let guard = Notify::new();
+    guard.notified().await;
 }
