38356dd229ea6ca8392613ae703931141a357b71
Santiago Pardal <santiagopardal@outlook.com>
2024-04-24 16:54:57 -0300

    Route all cameras to my queue

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,11 @@ use amqprs::{
 };
 use tokio::sync::Notify;
 use tracing_subscriber::{fmt, prelude::*};
-pub mod consumer;
 use consumer::temporal_videos_consumer::TemporalVideosConsumer;
+
+pub mod consumer;
+pub mod api;
+pub mod structs;
 
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
@@ -25,6 +28,8 @@ async fn main() {
     let rabbit_host = env::var("RABBIT_HOST").unwrap();
     let rabbit_user = env::var("RABBIT_USER").unwrap();
     let rabbit_password = env::var("RABBIT_PASSWORD").unwrap();
+
+    let cameras: Vec<structs::camera::Camera> = api::cameras::get_all_cameras().await;
 
     tracing_subscriber::registry()
         .with(fmt::layer())
@@ -59,16 +64,18 @@ async fn main() {
         .unwrap()
         .unwrap();
 
-    let routing_key = "amqprs.example";
-    let exchange_name = "test_exchange";
-    channel
-        .queue_bind(QueueBindArguments::new(
-            &queue_name,
-            exchange_name,
-            routing_key,
-        ))
-        .await
-        .unwrap();
+    for camera in cameras {
+        let exchange_name = "test_exchange";
+        println!("Binding camera '{}' with id: {}", camera.name, camera.id);
+        channel
+            .queue_bind(QueueBindArguments::new(
+                &queue_name,
+                exchange_name,
+                &camera.id.to_string(),
+            ))
+            .await
+            .unwrap();
+    }
 
     let args = BasicConsumeArguments::new(&queue_name, "basic_consumer")
         .manual_ack(true)
